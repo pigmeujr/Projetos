5153a3d202146da07c25f4e4ae5f420e413d63cc
yan <yanpettenepinto@gmail.com>
2026-03-19 11:51:50 -0300

    Delete Prog I directory

--- Prog I/Aula/aula04.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int idade;
-    printf("Digite a idade da criança \n");
-    scanf("%d",&idade);
-    idade>12 ? printf("A criança pode usar o brinquedo do parque"): printf("A criança não pode usar o brinquedo do parque");
-}
--- Prog I/Lista01/ex9.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main () {
-    int hora,minuto,mins;
-    printf("Digite o numero de horas e o numero de minutos: ");
-    scanf("%d %d",&hora,&minuto);
-    mins = (hora*60)+minuto;
-    printf("Tempo decorrido %d",mins);
-}
